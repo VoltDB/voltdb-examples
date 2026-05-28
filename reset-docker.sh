d66f7c6e92e4d2f941f14461c9a9c1067c3d7223
Anish Khanzode <akhanzode@voltactivedata.com>
2026-05-28 09:34:40 -0400

    Cleanup

--- reset-docker.sh
+++
@@ -1,8 +0,0 @@
-sudo rm -rf /Applications/Docker.app
-rm -rf ~/Library/Containers/com.docker.docker
-rm -rf ~/Library/Application\ Support/Docker\ Desktop
-rm -rf ~/Library/Group\ Containers/group.com.docker
-rm -rf ~/.docker
-rm -rf ~/Library/Logs/Docker\ Desktop
-rm -rf ~/Library/Caches/com.docker.docker
-rm -rf ~/Library/Preferences/com.docker.docker.plist
